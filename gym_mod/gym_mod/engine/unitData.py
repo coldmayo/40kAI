43b1ea1e29415014997322448b6c954bb1e32fa8
coldmayo <wallachm@msu.edu>
2024-04-20 18:01:13 -0400

    making better weapon system

--- gym_mod/gym_mod/engine/unitData.py
+++ gym_mod/gym_mod/engine/unitData.py
@@ -4,10 +4,19 @@ import os
 def unitData(army, unitName):
     with open(os.path.abspath("gym_mod/gym_mod/engine/unitData.json")) as j:
         data = json.loads(j.read())
-    for i in data["Data"]:
+    for i in data["UnitData"]:
         if i["Army"].lower() == army.lower() and i["Name"].lower() == unitName.lower():
             return i
-    print("Unit Not Available")
+    print("Unit Not Found")
+    return {}
+
+def weaponData(name):
+    with open(os.path.abspath("gym_mod/gym_mod/engine/unitData.json")) as j:
+        data = json.loads(j.read())
+    for i in data["WeaponData"]:
+        if i["Name"].lower() == name.lower():
+            return i
+    print("Weapon Not Found")
     return {}
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -2,16 +2,16 @@ import sys
 import numpy as np
 import gym
 from gym_mod.envs.warhamEnv import *
-from gym_mod.engine import genDisplay, Unit, unitData
+from gym_mod.engine import genDisplay, Unit, unitData, weaponData
 
 b_len = 60
 b_hei = 44
 
-enemy1 = Unit(unitData("Space Marine", "Eliminator Squad"), {"Name":"Bolt Pistol","BS":3,"S":4,"AP":0,"Range": 6, "Damage": 1}, np.random.randint(0,b_len), np.random.randint(0,b_hei))
-model1 = Unit(unitData("Space Marine", "Eliminator Squad"), {"Name":"Bolt Pistol","BS":3,"S":4,"AP":0,"Range": 6, "Damage": 1}, np.random.randint(0,b_len), np.random.randint(0,b_hei))
+enemy1 = Unit(unitData("Space Marine", "Eliminator Squad"), weaponData("Bolt Pistol"), np.random.randint(0,b_len), np.random.randint(0,b_hei))
+model1 = Unit(unitData("Space Marine", "Eliminator Squad"), weaponData("Bolt Pistol"), np.random.randint(0,b_len), np.random.randint(0,b_hei))
 
-enemy2 = Unit(unitData("Space Marine", "Apothecary"), {"Name":"Bolt Pistol","BS":3,"S":4,"AP":0,"Range": 6, "Damage": 1}, np.random.randint(0,b_len), np.random.randint(0,b_hei))
-model2 = Unit(unitData("Space Marine", "Apothecary"), {"Name":"Bolt Pistol","BS":3,"S":4,"AP":0,"Range": 6, "Damage": 1}, np.random.randint(0,b_len), np.random.randint(0,b_hei))
+enemy2 = Unit(unitData("Space Marine", "Apothecary"), weaponData("Absolver Bolt Pistol"), np.random.randint(0,b_len), np.random.randint(0,b_hei))
+model2 = Unit(unitData("Space Marine", "Apothecary"), weaponData("Absolver Bolt Pistol"), np.random.randint(0,b_len), np.random.randint(0,b_hei))
 
 env = gym.make("40kAI-v0", enemy = [enemy1, enemy2], model = [model1, model2], b_len = b_len, b_hei = b_hei)
 
